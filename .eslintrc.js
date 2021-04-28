3f8756fae9846f4d11e1bbcd710db95908a940df
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-04-27 21:58:02 -0700

    chore(deps): fix buggy linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,20 @@
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'import/extensions': 'off',
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        paths: ['src'],
-      },
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.ts'],
-      },
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-  },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 };
