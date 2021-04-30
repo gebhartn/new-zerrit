7de4fbe7cdcf0649fee2cb41e26ae946281571cb
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-04-29 23:06:00 -0700

    style(format): prettier

--- src/middlewares/common.ts
+++ src/middlewares/common.ts
@@ -1,40 +1,32 @@
-import { urlencoded, json, Application } from 'express';
-import cors from 'cors';
-import compression from 'compression';
-import morgan, { StreamOptions } from 'morgan';
+import { urlencoded, json, Application } from "express";
+import cors from "cors";
+import compression from "compression";
+import morgan, { StreamOptions } from "morgan";
 
-import { logger } from '../utils';
+import { logger } from "../utils";
 
 const stream: StreamOptions = {
-  write: (msg: string) => logger.http(msg),
+  write: (msg: string) => logger.http(msg)
 };
 const skip = (): boolean => {
-  const env = process.env.NODE_ENV || 'development';
-  return env !== 'development';
+  const env = process.env.NODE_ENV || "development";
+  return env !== "development";
 };
 
-const morganConfig = morgan(
-  ':method :url :status :res[content-length] - :response-time ms',
-  { stream, skip },
-);
+const morganConfig = morgan(":method :url :status :res[content-length] - :response-time ms", { stream, skip });
 
-export const handleCors = (app: Application) => {
+export const handleCors = (app: Application): void => {
   app.use(cors({ credentials: true, origin: true }));
 };
-export const handleParsing = (app: Application) => {
+export const handleParsing = (app: Application): void => {
   app.use(urlencoded({ extended: true }));
   app.use(json());
 };
-export const handleCompression = (app: Application) => {
+export const handleCompression = (app: Application): void => {
   app.use(compression());
 };
-export const handleMorgan = (app: Application) => {
+export const handleMorgan = (app: Application): void => {
   app.use(morganConfig);
 };
 
-export const common = [
-  handleCors,
-  handleParsing,
-  handleMorgan,
-  handleCompression,
-];
+export const common = [handleCors, handleParsing, handleMorgan, handleCompression];
